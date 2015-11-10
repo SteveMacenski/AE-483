e83a25031acafd125b4d3794fd04610ac133fc5a
Steven Macenski <smacens2@TL-321D-10.ad.uillinois.edu>
2015-11-09 19:17:17 -0600

    removal of old codes

--- Lab 4/codes/codes/angle_diff.m
+++
@@ -1,3 +0,0 @@
-function a = angle_diff(a1,a2)
-a = mod((a1-a2) + 3*pi, 2*pi) - pi;
-end
--- Lab 4/codes/codes/angle_meas.m
+++
@@ -1,7 +0,0 @@
-function out=angle_meas(x)
-
-% Measurement function for Euler angles simuation
-
-bias = 0;
-sigma = 0;
-out = x(7:9)+randn(3,1).*sigma+bias;
